36e75e91e305941fa84a5755f3fb5857756435a6
downing <joy97q12@naver.com>
2021-07-06 17:46:40 +0900

    DB Connect

--- src/main/java/com/downing/jpa/Application.java
+++ src/main/java/com/downing/jpa/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.downing.*", exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = "com.downing.*")
+@EnableJpaRepositories("com.downing.jpa.**")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
